dd654ecac2a1d0248bb31c2d9c58962262143900
Ezekiel Lopez <ezekiel.lopez@mrjaffesclass.com>
2023-10-05 11:06:55 -0700

    Commit message

---
+++ Ifstatements/HelloWorld.java
@@ -0,0 +1,55 @@
+public class HelloWorld
+{
+
+    
+    
+    public void run() {   
+        
+        int a = 5;
+        int b = 2;
+        int c = 1;
+        int d = -8;
+        
+        
+            if (a == 5) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }    
+    
+            if (b == 2) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }    
+       
+            if (c != 1) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }        
+    
+            if (d == -8)  {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }        
+        
+        
+        if (d == -8 && a == 4) {
+            System.out.println("SUCCESS"); 
+        } else { 
+            System.out.println("FAILURE"); 
+        }
+
+
+        System.out.println("Hello World!");
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        HelloWorld hw = new HelloWorld();
+        hw.run();
+    }
+}
