33c7dd2954d04f0ab07dd560af39a18b027d8bb9
Ezekiel Lopez <ezekiel.lopez@mrjaffesclass.com>
2023-10-10 11:00:34 -0700

    Initial commit

--- Ifstatements/ForLoops.java
+++ Ifstatements/ForLoops.java
@@ -1,28 +1,29 @@
 public class ForLoops
 
+
 {
+
 
     
     
     public void run() {   
         
                
-       for (int i = 0; i >= 15; i++)  {
-            System.out.println("i is greater than or equal to "+i); 
-            
-       }
+           for (int i = 0; i < 15; i++)  {
+                System.out.println("i is less than" +i);   
+        }
     
      
-       for (int j = 10; j == 10;  j = j + (-2))  {
-            System.out.println("j is equal to " +j);
-        
-       } 
-       
-    
-       for (int k = 10; k == 10;   k = k + -(3))  {
-            System.out.println("k is equal to " +k); 
-           
-       } 
+               for (int j = 10; j >= 0; j -= 2) {
+            System.out.println("j is greater than or equal to " + j);
+        }
+
+
+           for (int k = 10; k >= 0; k -= 3) {
+        System.out.println("k is greater than or equal to " + k);
+        }
+
+
 
 
         System.out.println("Hello World!");
